431b841c146849632c0eee2a6d480ef286f9b231
Dipo <dipson2solid@yahoo.com>
2022-03-13 22:09:58 +0000

    modified user to postgres

--- CRUDOperation/settings.py
+++ CRUDOperation/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'employee',
-        'USER': 'Dipo',
+        'USER': 'postgres',
         'PASSWORD': 'Dipo-1234567',
         'HOST': '127.0.0.1',
         'PORT': '5432',
